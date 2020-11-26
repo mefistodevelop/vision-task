eeebdcfa6a5bfbe972154cafca9d02aa02f15e14
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-11-26 22:24:52 +0300

    feat: add interceptors

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,19 @@ const instance = axios.create({
   baseURL: 'http://erp.apptrix.ru/api/clients/',
 });
 
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export const userApi = {
   signup(payload) {
     return instance.post('create/', payload);
@@ -18,3 +31,17 @@ export const userApi = {
     return instance.get(id);
   },
 };
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status !== 401) {
+      return new Promise((resolve, reject) => reject(error));
+    }
+
+    const refreshToken = localStorage.getItem('refreshToken');
+    return userApi.refreshToken(refreshToken);
+  }
+);
