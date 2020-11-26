6c81f7c9108408d3aae7844a1e505ad429259aa4
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-11-26 20:06:07 +0300

    feat: add redirect to login page
    if user is unathorized he must be redirected
    from main page to login page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-export const Main = () => (
-  <div className="main">
-    <h1>Main page</h1>
-  </div>
-);
+export function Main() {
+  const isAuthorized = useSelector((state) => state.isAuthorized);
+
+  return (
+    <>
+      {!isAuthorized ? <Redirect to="/login" /> : ''}
+      <div className="main">
+        <h1>Main page</h1>
+      </div>
+    </>
+  );
+}
