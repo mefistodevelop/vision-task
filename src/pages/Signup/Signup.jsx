271967bb8565697619a3be7afa28b04404dcf020
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-11-26 23:43:38 +0300

    feat: add redirect to main page if user is authorized

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import './Signup.css';
 import { SignupForm } from './SignupForm/SignupForm';
 
-export const Signup = () => (
-  <div className="signup">
-    <h1 className="signup__title">Sign up</h1>
-    <SignupForm />
-  </div>
-);
+export function Signup() {
+  const isAuthorized = useSelector((state) => state.isAuthorized);
+  return (
+    <>
+      {isAuthorized ? <Redirect exact to="/" /> : ''}
+      <div className="signup">
+        <h1 className="signup__title">Sign up</h1>
+        <SignupForm />
+      </div>
+    </>
+  );
+}
