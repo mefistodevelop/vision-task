dba4e5688f8d5d40f82711704539c5fc3493c014
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-11-27 09:22:39 +0300

    refactor: improve interceptors

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,23 @@ const instance = axios.create({
   baseURL: 'http://erp.apptrix.ru/api/clients/',
 });
 
+export const userApi = {
+  signup(payload) {
+    return instance.post('create/', payload);
+  },
+  signin(payload) {
+    return instance.post('token/', payload);
+  },
+  refreshToken(payload) {
+    return instance.post('token/refresh/', payload).then((response) => {
+      localStorage.setItem('accessToken', response.data.access);
+    });
+  },
+  getData(id) {
+    return instance.get(id);
+  },
+};
+
 instance.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem('accessToken');
@@ -17,31 +34,24 @@ instance.interceptors.request.use(
   }
 );
 
-export const userApi = {
-  signup(payload) {
-    return instance.post('create/', payload);
-  },
-  signin(payload) {
-    return instance.post('token/', payload);
-  },
-  refreshToken(payload) {
-    return instance.post('token/refresh/', payload);
-  },
-  getData(id) {
-    return instance.get(id);
-  },
-};
-
 instance.interceptors.response.use(
   (response) => {
     return response;
   },
   (error) => {
-    if (error.response.status !== 401) {
-      return new Promise((resolve, reject) => reject(error));
+    if (error.response.status === 401) {
+      debugger;
+      const refreshToken = localStorage.getItem('refreshToken');
+      try {
+        return userApi.refreshToken({ refresh: refreshToken });
+      } catch (err) {
+        localStorage.clear();
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      }
     }
 
-    const refreshToken = localStorage.getItem('refreshToken');
-    return userApi.refreshToken(refreshToken);
+    return new Promise((resolve, reject) => reject(error));
   }
 );
